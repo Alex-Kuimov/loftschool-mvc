f86b7879361a2ee4201db27810cb469a8e41f15c
Alex Kuimov <spoot@bk.ru>
2021-09-08 10:59:20 +0300

    fix

--- base/View.php
+++ base/View.php
@@ -2,7 +2,7 @@
 namespace Base;
 
 class View {
-	public function render( $templatePath, $data ){
+	public function render( $templatePath, $data=[] ){
 		ob_start();
 		include '../app/View/'.$templatePath;
 		return ob_get_clean();
