bb89f980e6cbcccdce1c19284d8b2837fe472fca
Alex Kuimov <spoot@bk.ru>
2021-09-11 11:41:25 +0300

    Add twig support

--- base/View.php
+++ base/View.php
@@ -2,9 +2,25 @@
 namespace Base;
 
 class View {
+
+	private $twig;
+
 	public function render( $templatePath, $data=[] ){
 		ob_start();
 		include '../app/View/'.$templatePath;
 		return ob_get_clean();
+	}
+
+	public function renderTwig( $templatePath, $data=[] ){
+		$parts = explode( '/', $templatePath );
+		$viewName = $parts[0];
+		$templateName = $parts[1];
+
+		if ( ! $this->twig ) {
+			$loader = new \Twig\Loader\FilesystemLoader( '../app/View/' . $viewName );
+			$this->twig = new \Twig\Environment( $loader );
+		}
+
+		return $this->twig->render( $templateName, $data );
 	}
 }
